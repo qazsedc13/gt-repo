a5b1c767a04dc3f18e17617a83370e748ce1c431
unknown <C:\Users\kolya\AppData\Roaming\The Bat!>
2015-12-17 22:23:19 +0600

    Создали кнопки Файл и О программе.

--- MyUtilites/MyUtilites/Form1.Designer.cs
+++ MyUtilites/MyUtilites/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MyUtilites
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.помощToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TSMIExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.TSMIAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.помощToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TSMIExit});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // помощToolStripMenuItem
+            // 
+            this.помощToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TSMIAbout});
+            this.помощToolStripMenuItem.Name = "помощToolStripMenuItem";
+            this.помощToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.помощToolStripMenuItem.Text = "Помощь";
+            // 
+            // TSMIExit
+            // 
+            this.TSMIExit.Name = "TSMIExit";
+            this.TSMIExit.Size = new System.Drawing.Size(152, 22);
+            this.TSMIExit.Text = "Выход";
+            this.TSMIExit.Click += new System.EventHandler(this.TSMIExit_Click);
+            // 
+            // TSMIAbout
+            // 
+            this.TSMIAbout.Name = "TSMIAbout";
+            this.TSMIAbout.Size = new System.Drawing.Size(152, 22);
+            this.TSMIAbout.Text = "О программе";
+            this.TSMIAbout.Click += new System.EventHandler(this.TSMIAbout_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Мои утилиты";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem TSMIExit;
+        private System.Windows.Forms.ToolStripMenuItem помощToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem TSMIAbout;
     }
 }
 
--- MyUtilites/MyUtilites/Form1.cs
+++ MyUtilites/MyUtilites/Form1.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace MyUtilites
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void TSMIExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TSMIAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Программа мои утилиты, содержит ряд небольших программ, которые могут пригодиться в жизни. \nА главное научить меня основам программирования на C#.\nАвтор Ковалёв Н.И.","О программе");
         }
     }
 }
